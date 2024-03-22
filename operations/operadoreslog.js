a92f5a495aab40cc6534175d6884b6fa4dbb5acc
s3llo <ssebasmurillo5@gmail.com>
2024-03-21 21:00:25 -0500

    jueves 21

---
+++ operations/operadoreslog.js
@@ -0,0 +1,13 @@
+console.log(10 >= 9);
+console.log(0===0);
+
+'midudev'.length //7
+'midudev'[1] // 1
+'midudev'.includes('dev')
+'midudev'.indexOf('midu') //
+'midudev'.startsWith('midu')
+'midudev'.endsWith('paint')
+'midudev'.slice(0,4)
+'midudev'.slice(4)
+'midudev'.toUpperCase()
+'midudev'.toLowerCase()
